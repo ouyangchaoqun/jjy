4a9beb82ca0282be92c645984e6cf898aeb92077
Ö캺ͦ <409199338@qq.com>
2017-09-20 15:46:53 +0800

    咨询师页面详情

--- public/src/component/answer/detail.vue
+++ public/src/component/answer/detail.vue
@@ -1,12 +1,157 @@
 <template id="myCenter">
-    <div style="height: 100%">
+    <div style="height: 100%" class="answer_detail_box">
 
-        <div v-title>抢答</div>
+
 
         <div style="height: 500px;">
+            <div class="answer_info">
+                <div class="answer_banner">
+                    <div class="answer_face"><img src="../../images/34.jpg"></div>
+                    <div class="answer_name">陈小刚</div>
+                    <div class="answer_cs">
+                        <div class="answer_ability">
+                            <span>情感困惑</span>
+                            <span>性心理</span>
+                            <span>亲子教育</span>
+                        </div>
+                    </div>
 
+                    <div class="answer_address">浙江-杭州</div>
+                </div>
+                <div class="answer_count">
+                    <div class="answer_countBox">
+                        <div class="counts">48</div>
+                        <div class="nr">个回答</div>
+                        <div class="line_1"></div>
+                    </div>
+                    <div class="answer_countBox">
+                        <div class="counts">824</div>
+                        <div class="nr">次偷听</div>
+                        <div class="line_1"></div>
+                    </div>
+                    <div class="answer_countBox">
+                        <div class="counts">99%</div>
+                        <div class="nr">好评率</div>
+                        <div class="line_1"></div>
+                    </div>
+                    <div class="answer_countBox">
+                        <div class="counts">135</div>
+                        <div class="nr">人收听</div>
+                    </div>
 
-race
+                </div>
+            </div>
+            <div class="answer_voice">
+                <div class="ts">帮你解决婚姻，情感中的困扰</div>
+                <div class="voice">
+                    <span class="hello">您好：</span>
+                    <div class="audio_btn">
+                        点击播放
+                    </div>
+                    <span class="minute">58"</span>
+                </div>
+            </div>
+            <div class="answer_detail">
+                <div class="answer_title">详细介绍</div>
+                <div class="content">
+                    <br>
+                    <p>国家二级心理咨询师。专注于婚恋情感咨询，擅长解决恋
+                        爱抑郁、恐惧症状的来访者走出困境；帮助过众多婚姻庭
+                        和恋爱过程中出现沟通冲突、处于亲密关系瓦解边缘的家
+                        庭化解危机；指导过众多工作中出现困惑的人做好职业规
+                        划。本人曾在蓝心网、给力心理从事心理咨询。希望我能
+                        陪你一起探索你的心理困惑，助你过上成功快乐，幸福美
+                        满的生活。</p>
+                    <br>
+                    <br>
+                    <h4>专业培训经验</h4>
+                    <p>
+                        武汉大学心理咨询培训<br>
+                        丁健婚恋咨询升级路径<br>
+                        中级绘画治疗师
+                    </p>
+                    <br>
+                    <br>
+                    <h4>专业培训经验</h4>
+                    <p>家庭关系：婚姻挽救、情感修复、外遇分离、婆媳关系、
+                        离婚纠纷
+                        <br>
+                        恋爱关系：失恋帮助，性格不和、沟通障碍
+                        性心理：性取向问题</p>
+                </div>
+                <div class="btn_sq">收起</div>
+            </div>
+            <div class="answer_comments">
+                <div class="answer_title">最新评价(311)</div>
+                <div class="list">
+                    <div class="item" >
+                        <div class="img"><img
+                                src="http://g.hiphotos.baidu.com/exp/w=480/sign=0b2f2cb8972397ddd679990c6982b216/f2deb48f8c5494ee9e081a462bf5e0fe99257e42.jpg">
+                        </div>
+                        <div class="info">
+                            <div class="name">陈**</div>
+                            <div class="star"><span class="on"></span><span class="on"></span><span></span><span></span><span></span></div>
+                            <div class="word">老师很负责，很用心，赞一个
+                                很专业，态度很好
+                            </div>
+                            <div class="class_s">
+                                <span>情感困惑</span>
+                                <span>性心理</span>
+                                <span>情感困惑</span>
+                                <div class="clear"></div>
+                            </div>
+                            <div class="time">2017-09-11</div>
+                        </div>
+                        <div class="clear"></div>
+                    </div>
+
+                </div>
+                <div class="btn_sq">查看更多评价</div>
+            </div>
+            <div class="ask_answer">
+                <div class="answer_title">问答(31) <div class="new">最新<span class="sj"></span></div></div>
+                <div class="list">
+                    <div class="item" style="margin-bottom: 0.88rem;padding-bottom: 1.23rem" v-for="item in [1,2]" >
+                        <div class="question">
+                            <div class="img"><img src="http://g.hiphotos.baidu.com/exp/w=480/sign=0b2f2cb8972397ddd679990c6982b216/f2deb48f8c5494ee9e081a462bf5e0fe99257e42.jpg">
+                            </div>
+                            <div class="info">
+                                <div class="word">
+                                    陈老师，六年的感情真的败给时间，六年的感情
+                                    真的败给时间，六年的感情真的败给时间，六年
+                                    的感情真的败给时间，六年的感情给时间，六...
+                                </div>
+                                <div class="price">￥19.9</div>
+                            </div>
+                            <div class="clear"></div>
+                        </div>
+                        <div class="answer">
+                            <div class="img"><img src="http://g.hiphotos.baidu.com/exp/w=480/sign=0b2f2cb8972397ddd679990c6982b216/f2deb48f8c5494ee9e081a462bf5e0fe99257e42.jpg">
+                            </div>
+                            <div class="info">
+                                    <div class="reply">
+                                    <div class="audio_btn">
+                                        一元偷听
+                                    </div>
+                                    <span class="minute">58"</span>
+                                       </div>
+                                <div class="status">
+                                    <div class="ask_time">刚刚</div>
+                                    <div class="ask_status">听过 148 <span><img src="../../images/answer/good.png"></span><span>48</span></div>
+                                </div>
+                            </div>
+                            <div class="clear"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="ask_bottom">
+                <div class="listen">
+                    <img src="../../images/answer/listen.png" alt=""/>
+                    <span>收听</span>
+                </div>
+                <div class="pay_ask">￥19.99提问</div>
+            </div>
         </div>
     </div>
 </template>
@@ -30,5 +175,214 @@ race
     }
 </script>
 <style>
+    .answer_detail_box .answer_info{
+        width: 100%;
+        height: 16.6rem;
+    }
+    .answer_detail_box .answer_banner{
+        background:#00B8E6;
+        height: 12.91rem;
+        padding-top: 0.88rem;
+    }
+    .answer_detail_box .answer_face{
+        width: 5.29rem;
+        height: 5.29rem;
+        border-radius: 50%;
+        margin: 0 auto;
+    }
+    .answer_detail_box  .answer_face img{
+        width: 100%;
+        border-radius: 50%;
+    }
+    .answer_detail_box .answer_name{
+        margin-top: 16px;
+        font-size: 18px;
+        line-height: 1;
+        color: white;
+        text-align: center;
+    }
+    .answer_detail_box .answer_cs{
+        margin-top: 14px;
+        text-align: center;
+    }
+    .answer_detail_box  .answer_ability{
+      display: inline-block;
+    }
+    .answer_detail_box  .answer_ability span{
+        display: block;
+        float:left;
+        text-align: center;
+        border-radius:0.6176470588235294rem;
+        height: 1.235294117647059rem;
+        line-height: 1.235294117647059rem;
+        border: 1px solid rgba(211,213,215,1);
+        color:rgba(211,213,215,1);
+        font-size: 0.7058823529411765rem;
+        padding: 0 0.7rem;
+        margin-right:0.55rem;
+    }
+    .answer_detail_box .answer_address{
+        margin-top: 4px;
+        text-align: center;
+        color: rgba(211,213,215,1);
+        line-height: 1;
+    }
+    .answer_detail_box .answer_count{
+        display: flex;
+        background: white;
+    }
+    .answer_detail_box   .answer_countBox{
+        width: 100%;
+        text-align: center;
+        position: relative;
+    }
+    .answer_detail_box .counts{
+        font-size:1.05rem;
 
+        color:rgba(102,102,102,1);
+    }
+    .answer_detail_box .nr{
+        font-size:0.705rem;
+
+        color:rgba(149,149,149,1);
+        line-height: 1;
+        margin-top: 0.1rem;
+        margin-bottom: 0.64rem;
+    }
+    .answer_detail_box .line_1{
+        width: 1px;
+        height: 1rem;
+        background:rgba(219,219,219,1);
+        position: absolute;
+        right: 0;
+        top:50%;
+        margin-top: -0.5rem;
+    }
+    .answer_detail_box .answer_voice{
+        height: 6.18rem;
+        margin-top: 0.88rem;
+        background: white;
+        padding-bottom: 0.88rem;
+    }
+    .answer_detail_box .ts{
+        padding-top: 0.88rem;
+        padding-left: 0.88rem;
+        font-size:0.88rem;
+
+        color:rgba(51,51,51,1);
+    }
+    .answer_detail_box .voice{
+        margin-top: 0.88rem;
+    }
+    .answer_detail_box .audio_btn{   width:50%; float:left; position: relative;
+        height:2.4rem; line-height: 2.4rem;
+        background:rgba(0,184,230,1);
+        border-radius: 1.2rem ; text-align: center; color:#fff;  }
+    .answer_detail_box  .audio_btn:active{ background: rgb(0, 138, 182);
+    }
+    .answer_detail_box .hello{
+        padding-left: 0.88rem;
+        color:#999; font-size: 1rem;
+        float: left;
+        margin-top: 0.88rem;
+    }
+    .answer_detail_box .minute{ color:#999; font-size: 1rem; margin-left: 0.8rem; float:left; line-height: 2.4rem;}
+    .answer_detail_box .audio_btn:before{ width: 0.6764705882352941rem; position: absolute;
+        content:" "; display: block; height: 0.97rem; background: url(../../images/sond.png); background-size: 0.6764705882352941rem;  top:0.6rem; left:0.8rem;}
+    .answer_detail_box  .audio_btn:after {
+        content: " ";
+        display: block;
+        position: absolute;
+        bottom: -0.6rem;
+        left: 0.9rem;
+        width: 0;
+        height: 0;
+        border-top: 0.6764705882352941rem solid #00B8E6;
+        border-right: 0.6764705882352941rem solid transparent;
+    }
+    .answer_detail_box .answer_detail{
+        background: white;
+        margin-top: 0.88rem;
+        padding-bottom: 1.2rem;
+    }
+    .answer_detail_box .answer_title{
+        height: 2.59rem;
+        line-height: 2.59rem;
+        font-size: 0.88rem;
+        padding-left: 0.88rem;
+        border-bottom: 1px solid #EEEEEE;
+        background: white;
+    }
+    .answer_detail_box .content{
+        font-size: 13px;
+        padding:0.88rem ;
+    }
+    .answer_detail_box .btn_sq{
+        width:5.235rem;
+        height:1.33rem;
+        border-radius: 0.66rem ;
+        border: 1px solid #0BB20C;
+        margin:0 auto;
+        text-align: center;
+        color: #0BB20C;
+        line-height: 1.33rem;
+        font-size: 0.64rem;
+    }
+    .answer_detail_box .answer_comments{
+        background: white;
+        margin-top: 0.88rem;
+        padding-bottom: 0.88rem;
+    }
+   .answer_detail_box  .list .item{ background: #fff; padding: 0.8823529411764706rem 0.8823529411764706rem 0 0.8823529411764706rem;  }
+    .answer_detail_box .list .star span{ background: url(../../images/answer/star_no.png); width: 0.7647058823529412rem; height: 0.7647058823529412rem;  background-size: 0.7647058823529412rem; display: inline-block; margin-right: 0.3rem; }
+    .answer_detail_box  .list .star span.on{background: url(../../images/answer/star.png);background-size: 0.7647058823529412rem; }
+
+    .answer_detail_box  .list .item .img{ width: 2rem; float:left; }
+    .answer_detail_box  .list .item .img img{ width: 100%; border-radius: 50%; border: 1px solid #EEEAEA }
+    .answer_detail_box  .list .info{ float:left; margin-left:0.8823529411764706rem;  width: 83%}
+    .answer_detail_box  .list .info .name{ font-size: 0.7058823529411765rem; color:#999; margin-bottom: 0.2rem; width: 14.11764705882353rem;}
+    .answer_detail_box  .list .word{ font-size:0.7647058823529412rem;  color:#333; margin-bottom: 0.2rem;;}
+    .answer_detail_box  .list .time{ font-size:0.7058823529411765rem; color:#C1C1C1; margin-top: 0.6rem;margin-bottom: 0.6rem;}
+    .answer_detail_box  .list .info .class_s { clear: both; margin-top: 0.3rem;}
+    .answer_detail_box  .list .info .class_s span{ display: block; float:left; text-align: center; border-radius:0.6176470588235294rem;  height: 1.235294117647059rem; line-height: 1.235294117647059rem;  border: 1px solid #C1C1C1 ; color:#C1C1C1 ; font-size: 0.7058823529411765rem; padding: 0 0.7rem; margin-right:0.55rem;}
+    .answer_detail_box .ask_answer{
+        margin-top: 0.88rem;
+    }
+    .answer_detail_box .sj{
+        width: 0;
+        height: 0;
+        border-top: 5px solid #999999 ;
+        border-right: 5px solid transparent;
+        border-left: 5px solid transparent;
+        position: absolute;
+        top:1.17rem;
+        margin-left: 0.294rem;
+    }
+    .answer_detail_box .new{float: right;margin-right: 0.58rem; position: relative; padding-right: 1.17rem; color: #999999 ;}
+    .answer_detail_box .price{
+        font-size: 0.74rem;
+        color: #666;
+    }
+    .answer_detail_box .reply{height: 50px;}
+    .answer_detail_box .status{color: #999999; font-size:0.70rem;margin-top: 0.58rem }
+    .answer_detail_box .ask_time{float: left;}
+    .answer_detail_box .answer{margin-top: 0.88rem}
+    .answer_detail_box .ask_status{float: right;}
+    .answer_detail_box .ask_status img{width: 0.94rem;}
+    .answer_detail_box .ask_status span{margin: 0 5px;}
+    .answer_detail_box .ask_bottom{
+        margin-top: 2rem;
+        height: 2.588rem;
+        text-align: center;
+    }
+    .answer_detail_box .listen{
+        float: left;
+        width: 25%;
+        background: white;
+        box-shadow: #eee 0 -5px 5px -5px;
+        height: 2.588rem;
+    }
+    .answer_detail_box .listen span{color: #999;}
+    .answer_detail_box .listen img{width: 1.05rem;height: 1.05rem;margin-top: 0.70rem}
+    .answer_detail_box .pay_ask{width: 75%;float: right; background: #0BB20C;color: white;line-height: 2.588rem;height: 2.588rem}
 </style>
