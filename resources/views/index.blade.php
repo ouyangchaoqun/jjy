f61aed31069221fb0e8cbf503f41686c2a50bd62
朱重熊 <308083469@qq.com>
2017-09-20 11:34:26 +0800

    配置

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,17 +6,17 @@
   <meta name="viewport" content="width=device-width,height=device-height,inital-scale=1.0,maximum-scale=1.0,user-scalable=no;">
   <title>心情指数</title>
   <link rel="stylesheet" type="text/css" href="https://res.wx.qq.com/open/libs/weui/1.1.2/weui.min.css"/>
-  <link rel="stylesheet" type="text/css" href="src/css/common.css?v=<?=STATIC_XQZS_VERSION?>"/>
-  <link rel="stylesheet" href="src/css/swiper-3.4.2.min.css">
-  <script src="http://lib.sinaapp.com/js/jquery/3.1.0/jquery-3.1.0.min.js"></script>
-  <script src="src/js/vue/vue.min.js"></script>
-  <script src="src/js/vue/vue-resource.js"></script>
-  <script src="src/js/vue/vue-router.min.js"></script>
-  <script src="src/js/swiper-3.3.1.jquery.min.js"></script>
-  <script src="src/js/swiper-3.4.2.min.js"></script>
-  <script src="src/js/weui.js"></script>
-  <script src="https://res.wx.qq.com/open/js/jweixin-1.2.0.js" type="text/javascript"></script>
-  <script src="http://map.qq.com/api/js?v=2" charset="utf-8"></script>
+  <link rel="stylesheet" type="text/css" href="/src/css/common.css?v=<?=STATIC_XQZS_VERSION?>"/>
+    <link rel="stylesheet" href="https://cdn.staticfile.org/Swiper/3.4.2/css/swiper.min.css">
+    <script  src="https://cdn.staticfile.org/jquery/3.0.0/jquery.min.js"></script>
+    <script src="https://cdn.staticfile.org/vue/2.2.6/vue.min.js"></script>
+    <script src="https://cdn.staticfile.org/vue-resource/1.3.1/vue-resource.min.js"></script>
+    <script src="https://cdn.staticfile.org/vue-router/2.4.0/vue-router.min.js"></script>
+    <script src="https://cdn.staticfile.org/Swiper/3.3.1/js/swiper.jquery.min.js"></script>
+    <script src="https://cdn.staticfile.org/Swiper/3.4.2/js/swiper.min.js"></script>
+    <script src="https://res.wx.qq.com/open/libs/weuijs/1.1.1/weui.min.js"></script>
+    <script src="https://res.wx.qq.com/open/js/jweixin-1.2.0.js" type="text/javascript"></script>
+    <script src="http://map.qq.com/api/js?v=2" charset="utf-8"></script>
 
 </head>
 <body>
--- resources/views/index_testing.blade.php
+++ resources/views/index_testing.blade.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width,inital-scale=1.0,maximum-scale=1.0,user-scalable=no;">
   <title>心情指数</title>
   <link rel="stylesheet" type="text/css" href="https://res.wx.qq.com/open/libs/weui/1.1.2/weui.min.css"/>
-  <link rel="stylesheet" type="text/css" href="src/css/common.css?v=<?=STATIC_XQZS_VERSION?>"/>
+  <link rel="stylesheet" type="text/css" href="/src/css/common.css?v=<?=STATIC_XQZS_VERSION?>"/>
   <link rel="stylesheet" href="https://cdn.staticfile.org/Swiper/3.4.2/css/swiper.min.css">
   <script  src="https://cdn.staticfile.org/jquery/3.0.0/jquery.min.js"></script>
   <script src="https://cdn.staticfile.org/vue/2.2.6/vue.min.js"></script>
@@ -29,7 +29,6 @@
     web.BASE_PATH2="<?=env("BASE_PATH")?>";
     web.BASE_PATH="<?=env("BASE_PATH")?>";
     web.API_PATH=web.BASE_PATH2+"api/";
-    web.IPAddress="<?=$_SERVER["REMOTE_ADDR"]?>";
     web.IMG_PATH="/dist/";
     var now_week= "<?=date("W")?>";
     <?php
