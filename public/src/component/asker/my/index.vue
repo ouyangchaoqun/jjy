9a1d6f335773e6cdb11729f3078f9bac1156d7f4
朱重熊 <308083469@qq.com>
2017-09-21 12:08:31 +0800

    我的

--- public/src/component/asker/my/index.vue
+++ public/src/component/asker/my/index.vue
@@ -1,6 +1,7 @@
 <template>
     <div style="height: 100%" class="asker_my_index_box">
         <div v-title>我的</div>
+        <div class="main">
         <div class="top">
                 <img class="img" src="http://g.hiphotos.baidu.com/exp/w=480/sign=0b2f2cb8972397ddd679990c6982b216/f2deb48f8c5494ee9e081a462bf5e0fe99257e42.jpg">
                 <div class="name">
@@ -9,14 +10,14 @@
             <div class="clear"></div>
 
         </div>
-        <router-link to = "../my/income" >我的收益
+        <router-link to = "../my/income"  class="income"  >我的收益
             <div class="price">￥7.5</div>
         </router-link>
-        <router-link to = "../my/income" >我的收益</router-link>
-        <router-link to = "../my/listen/list" >我的偷听</router-link>
-        <router-link to = "../my/answer/list" >我的收听</router-link>
-        <router-link to = "../my/comment/list" >我的评价</router-link>
-
+        <router-link to = "../my/listen/list" class="listen" >我的偷听</router-link>
+        <router-link to = "../my/answer/list" class="answer" >我的收听</router-link>
+        <router-link to = "../my/comment/list" class="comment" >我的评价</router-link>
+        </div>
+        <div class="join">入驻咨询师</div>
 
     </div>
 </template>
@@ -41,14 +42,25 @@
     }
 </script>
 <style>
-    .asker_my_index_box .top{ height:  2.588235294117647rem; line-height:  2.588235294117647rem; font-size: 0.88rem; background: #fff; padding: 0.88rem;}
-    .asker_my_index_box .top img{ display: block; float:left; width:2.588235294117647rem; height: 2.588235294117647rem; border-radius: 6px;  margin-right: 0.5rem;}
+    .asker_my_index_box .top{ height:  3.529411764705882rem; line-height:  3.529411764705882rem; font-size: 1.058823529411765rem; background: #fff; padding:0.8823529411764706rem;}
+    .asker_my_index_box .top img{ display: block; float:left; width:3.529411764705882rem; height: 3.529411764705882rem; border-radius: 50%;  margin-right: 0.98rem;}
 
-    .asker_my_index_box a{ position: relative; height:3.470588235294118rem;    line-height:3.470588235294118rem; background: #fff; margin-top: 0.4rem;
-     color:#333; font-size: 0.8823529411764706rem; padding:  0  0.8823rem;display: block }
+    .asker_my_index_box a{ position: relative; height:3.470588235294118rem;    line-height:3.470588235294118rem; background: #fff;
+     color:#333; font-size: 0.8823529411764706rem; padding:  0  0.8823rem;display: block; overflow: hidden; padding-left: 2.66rem; }
+    .asker_my_index_box a:nth-child(2){margin-top: 0.88235rem;}
     .asker_my_index_box a:after{ background: url(../../../images/arrow.png) ; width: 0.9411764705882353rem; height: 0.9411764705882353rem; background-size: 0.9411764705882353rem; position: absolute;
         right:0.88235rem;top:1.25rem ; content: " "}
+    .asker_my_index_box a:before{ content: " "; height: 0.0588235294117647rem; background: #eee; display: block; position: absolute; bottom:0;left: 0.9411764705882353rem; width: 100%}
     .asker_my_index_box a .price{position: absolute;
         right:2.2rem;top:0rem ;}
+    .asker_my_index_box a:last-child:before{
+        display: none;}
+
+    .asker_my_index_box a.income{ background: url(../../../images/asker/my_index_income.png) no-repeat #fff; background-size:1.117647058823529rem; background-position: 1rem  1.02rem; }
+    .asker_my_index_box a.listen{ background: url(../../../images/asker/my_index_listen.png) no-repeat #fff; background-size:0.9411764705882353rem; background-position: 1rem  1.02rem; }
+    .asker_my_index_box a.answer{ background: url(../../../images/asker/my_index_answer.png) no-repeat #fff; background-size:1.117647058823529rem; background-position: 1rem  1.02rem; }
+    .asker_my_index_box a.comment{ background: url(../../../images/asker/my_index_star.png) no-repeat #fff; background-size:1.117647058823529rem; background-position: 1rem  1.02rem; }
+
+    .join{ background: #fff; line-height:3.470588235294118rem; margin: 0.8823529411764706rem 0 ; text-align: center; font-size: 0.8823529411764706rem }
 
 </style>
