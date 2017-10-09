d3ea0fb26d440ea1f5e701d97fdee94da79c875a
朱重熊 <308083469@qq.com>
2017-10-09 10:22:04 +0800

    录音功能

--- public/src/component/answer/my/message.vue
+++ public/src/component/answer/my/message.vue
@@ -160,7 +160,7 @@
                         expertId:cookie.get("expertId"),
                         userId:"_userId_"
                     }
-                    _this.$http.put(web.API_PATH + "come/expert/modify/voice", data)
+                    _this.$http.post(web.API_PATH + "come/expert/modify/voice", data)
                         .then(function (bt) {
                             if (bt.data && bt.data.status == 1) {
                                 this.finish=true;
