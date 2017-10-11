662d0bd7642190b08d89f7635285b4b2dd0875ad
朱重熊 <308083469@qq.com>
2017-10-11 14:11:24 +0800

    ios audio

--- public/src/component/asker/my/ask_detail.vue
+++ public/src/component/asker/my/ask_detail.vue
@@ -101,11 +101,16 @@
             this.id= parseInt(this.$route.query.id);
             this.getDetail();
             this.getTags();
-
+            xqzs.voice.audio=null;
         },
         methods: {
-
+            initVoice:function () {
+                if(xqzs.voice.audio==null){
+                    xqzs.voice.audio=document.createElement("audio");
+                }
+            },
             play:function (index) {
+                this.initVoice();
                 let _this=this;
                 let list = _this.detail.answers;
                 //重置其他列表内容
