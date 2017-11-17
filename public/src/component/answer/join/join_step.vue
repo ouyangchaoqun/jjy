b9bd8769cb2e3d5f7cf6db4631ba3fa184d6ba07
ouyangchaoqun <ouyangchaoqun@aliyun.com>
2017-11-17 17:34:22 +0800

    bug

--- public/src/component/answer/join/join_step.vue
+++ public/src/component/answer/join/join_step.vue
@@ -55,7 +55,7 @@
                 <li>
                     <span class="li_left"></span>邮 箱
                     <div class="li_right">
-                        <input type="text" class="email" :value="email" @input="emailChange()">
+                        <input type="email" class="email" :value="email" @blur="fouceOut()">
                         <i></i>
                     </div>
                 </li>
@@ -73,7 +73,7 @@
                 <li>
                     <span class="li_left">*</span>身份证号
                     <div class="li_right">
-                        <input type="text" class="identityNo"  @input="idcardChange()" :value="identityNo" pattern="[0-9a-zA-Z]*">
+                        <input type="text" class="identityNo"  @input="idcardChange()" :value="identityNo" pattern="[0-9a-zA-Z]*" >
                         <i></i>
                     </div>
                 </li>
@@ -126,7 +126,9 @@
                 mobileVal:'123',
                 realName:'',
                 mobileBox:false,
-                idcard:''
+                idcard:'',
+                ema : /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+(.[a-zA-Z0-9_-])+/,
+
 
             }
         },
@@ -186,7 +188,7 @@
                         _this.user = eval(data.data.data);
                         console.log(_this.user)
                         _this.realName =  _this.user.realName
-                        _this.identityNo = _this.user.idcard
+                        _this.identityNo = cookie.get('identityNo')==null?_this.user.idcard:cookie.get('identityNo');
                         _this.sex=_this.user.sex==1?'男':'女';
                         _this.cardType=_this.user.cardType;
                         _this.email = _this.user.email;
@@ -234,6 +236,17 @@
             changeRealName:function () {
                 let realNameVal = $('.realName').val()
                 this.realName = realNameVal
+            },
+            fouceOut:function () {
+              console.log('shiqushiqu')
+                let _this = this;
+                let emailVal = $('.email').val()
+                if (!_this.ema.test(emailVal)){
+                    xqzs.weui.tip("请填写正确的邮箱");
+                }else {
+                    _this.email = emailVal
+                }
+                console.log(_this.email)
             },
             getSexPicker:function () {
                 let _this = this;
@@ -408,14 +421,21 @@
             idcardChange:function (v) {
                 let _this = this;
                 let identityNo = $(".identityNo").val();
-                _this.identityNo = identityNo
                 console.log( _this.identityNo )
+                cookie.set('identityNo',identityNo)
+                _this.identityNo = identityNo
             },
-            emailChange:function () {
-                let _this = this;
-                let emailVal = $('.email').val()
-                _this.email = emailVal
-            },
+//            emailChange:function () {
+//                let _this = this;
+//                let ema = /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+(.[a-zA-Z0-9_-])+/;
+//                let emailVal = $('.email').val()
+//                if (!ema.test(emailVal)){
+//                    xqzs.weui.tip("请填写正确的邮箱");
+//                }else {
+//                    _this.email = emailVal
+//                }
+//
+//            },
             goMobile:function () {
                 $('.mobile_box').show()
             },
@@ -440,6 +460,10 @@
             },
             msgSubmit: function () {
                 let _this = this;
+                if (!_this.ema.test(_this.email)){
+                    xqzs.weui.tip("请填写正确的邮箱");
+                    return
+                }
                 _this.showLoad= true;
                 let mobileVal = $('.li_right .mobile').text();
                 let msg = {
