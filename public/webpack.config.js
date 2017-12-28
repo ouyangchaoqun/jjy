42e619c93657d4e7c20e10e75ec2f7fc770bda2f
朱重熊 <308083469@qq.com>
2017-12-28 08:42:19 +0800

    加载先后问题

--- public/webpack.config.js
+++ public/webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
     entry: './src/main.js',
     output: {
         path: path.resolve(__dirname, './dist'),
-        publicPath: '/dist/',
+        publicPath: '/comeon/dist/',
         filename: 'build.js?[hash:5]', libraryTarget: 'umd',
         umdNamedDefine: true
     },
